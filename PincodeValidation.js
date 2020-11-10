f157016ce891cbdce76a05deea28174d8e47178d
aksaht1032 <akshat.30oct@gmail.com>
2020-11-11 00:05:09 +0530

    Added : Validate PIN code to not take first character as special or alphabet added

--- PincodeValidation.js
+++ PincodeValidation.js
@@ -1,6 +1,16 @@
 let pinCodeExpression = RegExp('^[1-9]{1}[0-9]{5}$');
 let samplePINCode = "400088";
+let samplePINCode2 = "A400088";
 
-// UC_1 Validate pincode digit pattern and length 
-let isPINCodeValid = pincodeExpression.test();
-console.log("PIN Code is valid : ",isPINCodeValid);
+// UC_1 Validate pincode digit pattern and length
+isPINCodeValid1(samplePINCode);
+
+// UC_2 Validate pincode, no special character or alphabet at the beginning
+isPINCodeValid1(samplePINCode2);
+
+
+function isPINCodeValid1(sample) {
+    let isPINCodeValid = pinCodeExpression.test(sample);
+    console.log(sample,"PIN Code is valid : ", isPINCodeValid);
+}
+
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ##### WELCOME TO PINCODE AND EMAIL VALIDATION PROGRAM
 
 ###### UC_1 Validate pincode digit pattern and length
+
+###### UC_2 Validate pincode, no special character or alphabet at the beginning
